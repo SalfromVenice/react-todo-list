9353009b4aaecf20ce823c676f9d871875979414
Salvatore Strangio <sal.strangio@gmail.com>
2022-01-14 18:29:26 +0100

    Changed App function component into a stateful class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,31 @@ import React from "react";
 import todosData from "../todosData";
 import ToDoItem from "./ToDoItem";
 
-const App = () => {
-  const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item}/>);
+// const App = () => {
+//   const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item}/>);
 
-  return (
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  );
-};
+//   return (
+//     <div className="todo-list">
+//       {todoItems}
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todoItems: todosData.map(item => <ToDoItem key={item.id} item={item} />)
+    }
+  }
+
+  render() {
+    return (
+      <div className="todo-list">
+        {this.state.todoItems}
+      </div>
+    )
+  }
+}
 
 export default App;
