73a2042b922916d6f7568fdaa134c67a9859e606
Salvatore Strangio <sal.strangio@gmail.com>
2022-01-14 19:56:57 +0100

    onChange actually change the checked status

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,34 @@ import React from "react";
 import todosData from "../todosData";
 import ToDoItem from "./ToDoItem";
 
-// const App = () => {
-//   const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item}/>);
-
-//   return (
-//     <div className="todo-list">
-//       {todoItems}
-//     </div>
-//   );
-// };
-
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      todoItems: todosData.map(item => <ToDoItem key={item.id} item={item} />)
+      todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
   }
 
   render() {
+    const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />)
     return (
       <div className="todo-list">
-        {this.state.todoItems}
+        {todoItems}
       </div>
     )
   }
