a2b49d0e8744357f33ee98bd243007904b5d5d9a
Salvatore Strangio <sal.strangio@gmail.com>
2022-01-14 12:38:13 +0100

    App, ToDoItem

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import ToDoItem from "./ToDoItem"
+
+const App = () => {
+  return (
+    <div className="todo-list">
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </div>
+  )
+}
+
+export default App
